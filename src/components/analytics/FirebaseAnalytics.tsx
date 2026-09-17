e45c2175b6e4c37fa9db864bd7d308627219bf04
Tobias Moreno <tobiasmoreno.tm.21@gmail.com>
2026-09-17 14:17:59 -0300

    Update FirebaseAnalytics.tsx

--- src/components/analytics/FirebaseAnalytics.tsx
+++ src/components/analytics/FirebaseAnalytics.tsx
@@ -6,7 +6,6 @@ import { firebaseApp } from "@/lib/firebase";
 export function FirebaseAnalytics() {
   useEffect(() => {
     let active = true;
-
     async function initializeAnalytics() {
       const { getAnalytics, isSupported } = await import("firebase/analytics");
       if (active && (await isSupported())) getAnalytics(firebaseApp);
